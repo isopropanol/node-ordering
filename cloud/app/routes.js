6e87d9600b0f0bd6147ff978ca9ebe0910e36380
Paul Ammann <meanmrclean@gmail.com>
2015-09-05 13:59:45 -0700

    querying menu items

--- cloud/app/routes.js
+++ cloud/app/routes.js
@@ -11,13 +11,36 @@
 // 		});
 // };
 
+var _ = require('underscore');
+
 module.exports = function(app) {
 
 	// api ---------------------------------------------------------------------
 
 	app.get('/api/menus', function(req, res) {
+		var menus;
 		var menuQuery = new Parse.Query("Menu");
-		menuQuery.find().then(function(menus){
+		menuQuery.find().then(function(data){
+			menus = data;
+			var promises = [];
+			menus.forEach(function(menu){
+				var menuItemQuery = new Parse.Query("MenuItem");
+				menuItemQuery.equalTo("menu",menu);
+				promises.push(menuItemQuery.find())
+			})
+			return Parse.Promise.when(promises);
+		}).then(function(){
+			var args = _.toArray(arguments);
+			args.forEach(function(menuItems,index){
+				console.log("we got "+menuItems.length);
+				menu = menus[index];
+				console.log("for: "+menu.get('name'));
+				menu.set('menuItems',JSON.parse(JSON.stringify(menuItems)));
+				console.log(menu)
+				menus[index] = menu;
+			})
+			console.log("we have "+menus.length+" menus");
+
 			res.json(menus)
 		})
 		
