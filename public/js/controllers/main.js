0709a70e472020ecc2a1ef80dccf56ad1dc4216f
Paul Ammann <meanmrclean@gmail.com>
2015-09-06 18:27:14 -0700

    initial modal setup

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,13 +1,15 @@
 angular.module('menuController', [])
 
 	// inject the Menu service factory into our controller
-	.controller('mainController', ['$scope','$http','Menus', function($scope, $http, Menus) {
+	.controller('mainController', ['$scope','$http','Menus',"ModalService", function($scope, $http, Menus, ModalService) {
 		$scope.formData = {};
 		$scope.loading = true;
 
 		// GET =====================================================================
 		// when landing on the page, get all menus and show them
 		// use the service to get all the menus
+		console.log(ModalService)
+
 		Menus.get()
 			.success(function(data) {
 				$scope.menus = data;
@@ -16,23 +18,34 @@ angular.module('menuController', [])
 
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
-		$scope.createMenu = function() {
+		$scope.createOrder = function(menuItem) {
 
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
-			if ($scope.formData.text != undefined) {
-				$scope.loading = true;
+			ModalService.showModal({
+				templateUrl: "views/orderModal.html",
+				controller: "modalController"
+			}).then(function(modal) {
+				console.log(modal)
+			//it's a bootstrap element, use 'modal' to show it
+				modal.element.modal();
+				modal.close.then(function(result) {
+					console.log(result);
+				});
+			});
+			// if ($scope.formData.text != undefined) {
+			// 	$scope.loading = true;
 
-				// call the create function from our service (returns a promise object)
-				Menus.create($scope.formData)
+			// 	// call the create function from our service (returns a promise object)
+			// 	Menus.create($scope.formData)
 
-					// if successful creation, call our get function to get all the new menus
-					.success(function(data) {
-						$scope.loading = false;
-						$scope.formData = {}; // clear the form so our user is ready to enter another
-						$scope.menus = data; // assign our new list of menus
-					});
-			}
+			// 		// if successful creation, call our get function to get all the new menus
+			// 		.success(function(data) {
+			// 			$scope.loading = false;
+			// 			$scope.formData = {}; // clear the form so our user is ready to enter another
+			// 			$scope.menus = data; // assign our new list of menus
+			// 		});
+			// }
 		};
 
 		// DELETE ==================================================================
@@ -47,4 +60,9 @@ angular.module('menuController', [])
 		// 			$scope.menus = data; // assign our new list of menus
 		// 		});
 		// };
-	}]);
+	}])
+	.controller("modalController",['$scope','$http','close', function($scope, $http,close) {
+		$scope.close = function(result) {
+			close(result, 500); // close, but give 500ms for bootstrap to animate
+		};
+	}])
